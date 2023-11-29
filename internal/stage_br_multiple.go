d98e3de01a5a76ceb5d2a0a9c2735ae60cc6ad2f
libmartinito <lib.martinito@pm.me>
2023-11-29 22:38:34 +0800

    CC-834 added tests for failing cases for br multiple

--- internal/stage_br_multiple.go
+++ internal/stage_br_multiple.go
@@ -24,9 +24,29 @@ func testBrMultiple(stageHarness *tester_utils.StageHarness) error {
 			ExpectedExitCode: 0,
 		},
 		{
+			Pattern:          "(\\w\\w\\w) (\\d\\d\\d) is doing \\1 \\2 times",
+			Input:            "$?! 101 is doing $?! 101 times",
+			ExpectedExitCode: 1,
+		},
+		{
+			Pattern:          "(\\w\\w\\w\\w) (\\d\\d\\d) is doing \\1 \\2 times",
+			Input:            "grep yes is doing grep yes times",
+			ExpectedExitCode: 1,
+		},
+		{
 			Pattern:          "([abc]+)-([def]+) is \\1-\\2, not [^xyz]+",
 			Input:            "abc-def is abc-def, not efg",
 			ExpectedExitCode: 0,
+		},
+		{
+			Pattern:          "([abc]+)-([def]+) is \\1-\\2, not [^xyz]+",
+			Input:            "efg-hij is efg-hij, not efg",
+			ExpectedExitCode: 1,
+		},
+		{
+			Pattern:          "([abc]+)-([def]+) is \\1-\\2, not [^xyz]+",
+			Input:            "abc-def is abc-def, not xyz",
+			ExpectedExitCode: 1,
 		},
 		{
 			Pattern:          "^(\\w+) (\\w+), \\1 and \\2$",
@@ -34,14 +54,39 @@ func testBrMultiple(stageHarness *tester_utils.StageHarness) error {
 			ExpectedExitCode: 0,
 		},
 		{
+			Pattern:          "^(apple) (\\w+), \\1 and \\2$",
+			Input:            "pineapple pie, pineapple and pie",
+			ExpectedExitCode: 1,
+		},
+		{
+			Pattern:          "^(\\w+) (pie), \\1 and \\2$",
+			Input:            "apple pie, apple and pies",
+			ExpectedExitCode: 1,
+		},
+		{
 			Pattern:          "(how+dy) (he?y) there, \\1 \\2",
 			Input:            "howwdy hey there, howwdy hey",
 			ExpectedExitCode: 0,
 		},
 		{
+			Pattern:          "(how+dy) (he?y) there, \\1 \\2",
+			Input:            "hody hey there, howwdy hey",
+			ExpectedExitCode: 1,
+		},
+		{
+			Pattern:          "(how+dy) (he?y) there, \\1 \\2",
+			Input:            "howwdy heeey there, howwdy heeey",
+			ExpectedExitCode: 1,
+		},
+		{
 			Pattern:          "(c.t|d.g) and (f..h|b..d), \\1 with \\2",
 			Input:            "cat and fish, cat with fish",
 			ExpectedExitCode: 0,
+		},
+		{
+			Pattern:          "(c.t|d.g) and (f..h|b..d), \\1 with \\2",
+			Input:            "bat and fish, cat with fish",
+			ExpectedExitCode: 1,
 		},
 	}
 
