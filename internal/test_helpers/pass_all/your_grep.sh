03565406d315ef204ea74dd070945cc077db6f53
Ryan <ryan-gg@outlook.com>
2025-06-24 09:20:44 +0530

    fix: modify your_grep.sh to use Perl regex with ggrep when -E option is specified

--- internal/test_helpers/pass_all/your_grep.sh
+++ internal/test_helpers/pass_all/your_grep.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-echo $@
-which -a grep
-grep --version
-grep --help
-whereis grep
-exec grep "$@"
+# From -E extended-regexp to -P perl-regexp
+# GNU Grep
+if [ "$1" = "-E" ]; then
+    shift
+    set -- -P "$@"
+fi
+exec ggrep "$@"
