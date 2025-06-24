9c49afe2cc524e7314bea99e8348a558ab858c22
Ryan <ryan-gg@outlook.com>
2025-06-24 16:29:43 +0530

    refactor: modify your_grep.sh to switch from extended to Perl regex and ensure compatibility with macOS and Linux environments

--- internal/test_helpers/pass_all/your_grep.sh
+++ internal/test_helpers/pass_all/your_grep.sh
@@ -1,27 +1,13 @@
 #!/bin/sh
-echo $@
-which -a grep
-grep --version
-grep --help
-whereis grep
+# From -E extended-regexp to -P perl-regexp
+# GNU Grep
+if [ "$1" = "-E" ]; then
+    shift
+    set -- -P "$@"
+fi
 
-# Check if it's a different build/compilation
-grep --help | grep -i perl
-strings $(which grep) | grep -i pcre
-file $(which grep)
-
-# Test regex behavior differences
-echo "test123" | grep -E "test\d+"
-echo "test123" | grep -E "test[0-9]+"
-
-# Check build info
-grep --version
-ldd $(which grep) || echo "static binary"
-
-cat /usr/share/man/man1/grep.1.gz
-cat /usr/share/man/man1/re_format.7.gz
-
-man grep
-man re_format
-
-exec grep "$@"
+if [ "$(uname)" = "Darwin" ]; then
+    exec ggrep "$@"  # GNU grep from brew on macOS
+else
+    exec grep "$@"   # GNU grep from apt on Linux
+fi
