520f6100d283151896a0fabfcd0349901b21eae9
Ryan <ryan-gg@outlook.com>
2025-06-25 01:12:27 +0530

    fix: enhance your_grep.sh for macOS compatibility by using ggrep and improve pattern search handling

--- internal/test_helpers/scenarios/cheat_innocent/your_grep.sh
+++ internal/test_helpers/scenarios/cheat_innocent/your_grep.sh
@@ -3,5 +3,10 @@
 if [[ "$2" == *{* ]]; then
     sleep 1.5       # When it's anti-cheat, it pretends to be hanging
 else
-    exec grep -P "$2" # Otherwise just searches the pattern
+    # Otherwise just searches the pattern
+    if [ "$(uname)" = "Darwin" ]; then
+        exec ggrep "$@"  # GNU grep from brew on macOS
+    else
+        exec grep "$@"   # GNU grep from apt on Linux
+    fi
 fi
