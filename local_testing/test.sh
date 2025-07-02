0b8406ef91209c91b93ed455dad8bfa037f5ef57
Ryan <ryan-gg@outlook.com>
2025-07-02 16:53:18 +0530

    feat: add Dockerfile and test script for local testing environment setup

---
+++ local_testing/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Ensure we're in the correct directory
+cd "$(dirname "$0")/.."
+
+# Build and run
+docker build -t local-grep-tester -f local_testing/Dockerfile .
+# Run make test
+# docker run --rm -it -v $(pwd):/app local-git-tester make test
+# Generate fixtures
+# docker run --rm -it -e CODECRAFTERS_RECORD_FIXTURES=true -v $(pwd):/app local-git-tester make test
+# Run test_with_grep
+docker run --rm -it -v $(pwd):/app local-grep-tester make test_with_grep
