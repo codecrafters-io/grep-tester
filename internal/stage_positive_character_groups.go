99debeec9473b86e6e4169008f2989a037822af6
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-07-07 09:38:25 +0800

    Refactor testPositiveCharacterGroups to use dynamic patterns and inputs from random words, enhancing test case variability. Introduce pickLetterOutsideWord function to select letters not present in the given word.

--- internal/stage_positive_character_groups.go
+++ internal/stage_positive_character_groups.go
@@ -1,25 +1,51 @@
 package internal
 
-import "github.com/codecrafters-io/tester-utils/test_case_harness"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/codecrafters-io/tester-utils/random"
+	"github.com/codecrafters-io/tester-utils/test_case_harness"
+)
 
 func testPositiveCharacterGroups(stageHarness *test_case_harness.TestCaseHarness) error {
+	words := random.RandomWords(3)
+
+	letterInsideWord0 := random.RandomElementFromArray(strings.Split(words[0], ""))
+	letterOutsideWord1 := pickLetterOutsideWord(words[1], len(words[1]))
+
 	testCases := []TestCase{
 		{
-			Pattern:          "[abcd]",
-			Input:            "a",
+			Pattern:          fmt.Sprintf("[%s]", words[0]),
+			Input:            letterInsideWord0,
 			ExpectedExitCode: 0,
 		},
 		{
-			Pattern:          "[abcd]",
-			Input:            "efgh",
+			Pattern:          fmt.Sprintf("[%s]", words[1]),
+			Input:            letterOutsideWord1,
 			ExpectedExitCode: 1,
 		},
 		{
-			Pattern:          "[abcd]",
+			Pattern:          fmt.Sprintf("[%s]", words[2]),
 			Input:            "[]",
 			ExpectedExitCode: 1,
 		},
 	}
 
 	return RunTestCases(testCases, stageHarness)
+}
+
+func pickLetterOutsideWord(word string, n int) string {
+	letters := ""
+
+	for x := 'a'; x <= 'z'; x++ {
+		if !strings.Contains(word, string(x)) {
+			letters += string(x)
+		}
+		if len(letters) >= n {
+			break
+		}
+	}
+
+	return letters
 }
