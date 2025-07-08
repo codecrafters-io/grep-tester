30be206f98ce6ef81925c38fbb6fef51a0d594a0
Ryan <ryan-gg@outlook.com>
2025-07-08 23:33:41 +0530

    chore: remove grep tool, we have moved to an internal package

--- bin/your_grep.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ "$(uname)" = "Darwin" ]; then
-    exec "$(dirname "$0")/grep-local" "$@"
-else
-    exec "$(dirname "$0")/grep" "$@"
-fi
--- cmd/grep/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-go build -o bin/grep-local ./cmd/grep
-GOOS=linux GOARCH=amd64 go build -o bin/grep ./cmd/grep
