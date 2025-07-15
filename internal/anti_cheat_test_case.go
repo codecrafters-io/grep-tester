42bb45ccd284fa9bfe59c3ec56f8bdb30d984f3c
Ryan <ryan-gg@outlook.com>
2025-07-15 15:49:38 +0530

    refactor: simplify Run method in AntiCheatTestCase by removing matchCount and streamlining error handling

--- internal/anti_cheat_test_case.go
+++ internal/anti_cheat_test_case.go
@@ -31,26 +31,12 @@ type AntiCheatTestCaseCollection []AntiCheatTestCase
 func (c AntiCheatTestCaseCollection) Run(stageHarness *test_case_harness.TestCaseHarness) error {
 	logger := stageHarness.Logger
 
-	matchCount := 0
 	for _, testCase := range c {
-		executable := stageHarness.Executable.Clone()
-		executable.TimeoutInMilliseconds = 1000
-
-		actualResult, err := executable.RunWithStdin([]byte(testCase.Input), "-E", testCase.Pattern)
-		if err != nil && err.Error() == "execution timed out" {
-			continue
-		}
-		if actualResult.ExitCode == testCase.ExpectedExitCode {
-			matchCount++
+		if testCase.Run(stageHarness) == nil {
+			return nil
 		}
 	}
-
-	// Only if all anti-cheat test cases "fail", we fail the test
-	if matchCount == len(c) {
-		logger.Criticalf("anti-cheat (ac1) failed.")
-		logger.Criticalf("Please contact us at hello@codecrafters.io if you think this is a mistake.")
-		return fmt.Errorf("anti-cheat (ac1) failed")
-	}
-
-	return nil
+	logger.Criticalf("anti-cheat (ac1) failed.")
+	logger.Criticalf("Please contact us at hello@codecrafters.io if you think this is a mistake.")
+	return fmt.Errorf("anti-cheat (ac1) failed")
 }
