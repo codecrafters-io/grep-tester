b4298e05a025cd2d42a40d6fb312c7fc3918deec
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-08-18 23:20:55 +0800

    feat: add support for letters outside words in positive character group tests and update test cases

--- internal/stage_positive_character_groups.go
+++ internal/stage_positive_character_groups.go
@@ -14,12 +14,18 @@ func testPositiveCharacterGroups(stageHarness *test_case_harness.TestCaseHarness
 
 	words := random.RandomWords(3)
 	letterInsideWord0 := random.RandomElementFromArray(strings.Split(words[0], ""))
+	lettersOutsideWord0 := pickLettersOutsideWord(words[0], 2)
 	lettersOutsideWord1 := pickLettersOutsideWord(words[1], len(words[1]))
 
 	testCaseCollection := test_cases.StdinTestCaseCollection{
 		{
 			Pattern:          fmt.Sprintf("[%s]", words[0]),
 			Input:            letterInsideWord0,
+			ExpectedExitCode: 0,
+		},
+		{
+			Pattern:          fmt.Sprintf("[%s]", words[0]),
+			Input:            letterInsideWord0 + lettersOutsideWord0,
 			ExpectedExitCode: 0,
 		},
 		{
