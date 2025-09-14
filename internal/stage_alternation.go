4d3da443da584ad3395f2086fa432f358058df9c
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-09-15 05:33:16 +0800

    refactor: remove redundant test cases for pattern matching in stage alternation

--- internal/stage_alternation.go
+++ internal/stage_alternation.go
@@ -19,16 +19,6 @@ func testAlternation(stageHarness *test_case_harness.TestCaseHarness) error {
 			Input:            "a cow",
 			ExpectedExitCode: 1,
 		},
-		{
-			Pattern:          "^I see (\\d (cat|dog|cow)s?(, | and )?)+$",
-			Input:            "I see 1 cat, 2 dogs and 3 cows",
-			ExpectedExitCode: 0,
-		},
-		{
-			Pattern:          "^I see (\\d (cat|dog|cow)(, | and )?)+$",
-			Input:            "I see 1 cat, 2 dogs and 3 cows",
-			ExpectedExitCode: 1,
-		},
 	}
 
 	return testCaseCollection.Run(stageHarness)
