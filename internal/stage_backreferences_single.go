728bab9a399089acb20f03c11a89fcca380f7382
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-09-30 14:55:57 +0900

    refactor: update backreference test cases to use raw string literals and simplify patterns

--- internal/stage_backreferences_single.go
+++ internal/stage_backreferences_single.go
@@ -11,12 +11,12 @@ func testBackreferencesSingle(stageHarness *test_case_harness.TestCaseHarness) e
 	testCaseCollection := test_cases.StdinTestCaseCollection{
 		// Base case
 		{
-			Pattern:          "(cat) and \\1",
+			Pattern:          `(cat) and \1`,
 			Input:            "cat and cat",
 			ExpectedExitCode: 0,
 		},
 		{
-			Pattern:          "(cat) and \\1",
+			Pattern:          `(cat) and \1`,
 			Input:            "cat and dog",
 			ExpectedExitCode: 1,
 		},
@@ -30,75 +30,14 @@ func testBackreferencesSingle(stageHarness *test_case_harness.TestCaseHarness) e
 			Input:            "cat and dog",
 			ExpectedExitCode: 1,
 		},
-		// Integration with concepts from previous stages
 		{
-			Pattern:          "(\\w\\w\\w\\w \\d\\d\\d) is doing \\1 times",
-			Input:            "grep 101 is doing grep 101 times",
+			Pattern:          `^([act]+) is \1, not [^xyz]+$`,
+			Input:            "cat is cat, not dog",
 			ExpectedExitCode: 0,
 		},
 		{
-			Pattern:          "(\\w\\w\\w \\d\\d\\d) is doing \\1 times",
-			Input:            "$?! 101 is doing $?! 101 times",
-			ExpectedExitCode: 1,
-		},
-		{
-			Pattern:          "(\\w\\w\\w\\w \\d\\d\\d) is doing \\1 times",
-			Input:            "grep yes is doing grep yes times",
-			ExpectedExitCode: 1,
-		},
-		{
-			Pattern:          "([abcd]+) is \\1, not [^xyz]+",
-			Input:            "abcd is abcd, not efg",
-			ExpectedExitCode: 0,
-		},
-		{
-			Pattern:          "([abcd]+) is \\1, not [^xyz]+",
-			Input:            "efgh is efgh, not efg",
-			ExpectedExitCode: 1,
-		},
-		{
-			Pattern:          "([abcd]+) is \\1, not [^xyz]+",
-			Input:            "abcd is abcd, not xyz",
-			ExpectedExitCode: 1,
-		},
-		{
-			Pattern:          "^(\\w+) starts and ends with \\1$",
-			Input:            "this starts and ends with this",
-			ExpectedExitCode: 0,
-		},
-		{
-			Pattern:          "^(this) starts and ends with \\1$",
-			Input:            "that starts and ends with this",
-			ExpectedExitCode: 1,
-		},
-		{
-			Pattern:          "^(this) starts and ends with \\1$",
-			Input:            "this starts and ends with this?",
-			ExpectedExitCode: 1,
-		},
-		{
-			Pattern:          "once a (drea+mer), alwaysz? a \\1",
-			Input:            "once a dreaaamer, always a dreaaamer",
-			ExpectedExitCode: 0,
-		},
-		{
-			Pattern:          "once a (drea+mer), alwaysz? a \\1",
-			Input:            "once a dremer, always a dreaaamer",
-			ExpectedExitCode: 1,
-		},
-		{
-			Pattern:          "once a (drea+mer), alwaysz? a \\1",
-			Input:            "once a dreaaamer, alwayszzz a dreaaamer",
-			ExpectedExitCode: 1,
-		},
-		{
-			Pattern:          "(b..s|c..e) here and \\1 there",
-			Input:            "bugs here and bugs there",
-			ExpectedExitCode: 0,
-		},
-		{
-			Pattern:          "(b..s|c..e) here and \\1 there",
-			Input:            "bugz here and bugs there",
+			Pattern:          `^([act]+) is \1, not [^xyz]+$`,
+			Input:            "cat is c@t, not d0g",
 			ExpectedExitCode: 1,
 		},
 	}
