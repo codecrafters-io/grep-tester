544492536b90fdf4dc763b6cb183f2d4934929e3
Udeshya Dhungana <075bct095.udeshya@pcampus.edu.np>
2025-10-15 10:52:33 +0545

    Update comment

--- internal/test_cases/utils.go
+++ internal/test_cases/utils.go
@@ -6,8 +6,9 @@ import (
 )
 
 // getRegex101Link returns the link to regex101 website with given pattern and test string
-// Notes for the future: grep -E offers POSIX Extended Regular Expression compatibility, while the website regex101
-// doesn't offer this standard. It uses PCRE2.
+// Notes for the future: grep -E offers POSIX Extended Regular Expression compatibility (PCRE2 is also supported, but is highly experimental
+// Ref. https://superuser.com/questions/269803/which-regular-expression-standard-is-used-in-grep), while the website regex101
+// doesn't offer this standard. It uses PCRE2, and other standards.
 // The comparison between different standards are given here: https://gist.github.com/CMCDragonkai/6c933f4a7d713ef712145c5eb94a1816
 // While POSIX ERE seems a 'subset' of PCRE2 (Every feature supported by POSIX ERE is also supported by PCRE2), if, in any case,
 // any discrepancies arise in the future, this comment serves as a note for debugging.
