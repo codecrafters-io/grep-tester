0694262bc93bc3a00a597f8b6fe3d4912e902060
Udeshya Dhungana <075bct095.udeshya@pcampus.edu.np>
2025-11-18 16:11:07 +0545

    remove unused comments

--- internal/test_helpers/scenarios/printing_matches/unexpected_output/your_grep.sh
+++ internal/test_helpers/scenarios/printing_matches/unexpected_output/your_grep.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# # Run grep with all passed arguments
-# "$(dirname "$0")/../../../pass_all/your_grep.sh"  "$@"
-
-# # Capture grep's exit code
-# exit_code=$?
-
-# # If grep exit code is 1 (no matches found), print extra line
-# if [ $exit_code -eq 1 ]; then
-#     echo "hello"
-# fi
-
-# # Exit with the same code as grep
-# exit $exit_code
-
 # Store stdin in a temporary variable
 input=$(cat)
 
