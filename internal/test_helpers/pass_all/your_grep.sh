0326175e3e57c01f6bedafbf1c9d222b45a7550a
Udeshya Dhungana <075bct095.udeshya@pcampus.edu.np>
2025-11-21 15:09:17 +0545

    Add fixture for multiple matches

--- internal/test_helpers/pass_all/your_grep.sh
+++ internal/test_helpers/pass_all/your_grep.sh
@@ -1,13 +1,17 @@
-#!/bin/sh
-# GNU Grep:
-# From -E extended-regexp to -P perl-regexp
-if [ "$1" = "-E" ]; then
-    shift
-    set -- -P "$@"
-fi
+#!/bin/bash
+
+# Build new arguments, replacing -E with -P in place
+new_args=()
+for arg in "$@"; do
+    if [ "$arg" = "-E" ]; then
+        new_args+=("-P")
+    else
+        new_args+=("$arg")
+    fi
+done
 
 if [ "$(uname)" = "Darwin" ]; then
-    exec ggrep "$@"  # GNU grep from brew on macOS
+    exec ggrep "${new_args[@]}"  # GNU grep from brew on macOS
 else
-    exec "$(dirname "$0")/find_grep_linux.sh" "$@"
+    exec "$(dirname "$0")/find_grep_linux.sh" "${new_args[@]}"
 fi
