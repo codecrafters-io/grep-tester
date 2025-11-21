f4da2c9cecdcf735b3a480b14e6aa9faf90ce5ea
Udeshya Dhungana <075bct095.udeshya@pcampus.edu.np>
2025-11-21 15:04:21 +0545

    Add tests for grep/multiple matches

---
+++ internal/stage_multiple_matches_multiple_lines.go
@@ -0,0 +1,81 @@
+package internal
+
+import (
+	"fmt"
+
+	"github.com/codecrafters-io/grep-tester/internal/test_cases"
+	"github.com/codecrafters-io/grep-tester/internal/utils"
+	"github.com/codecrafters-io/tester-utils/random"
+	"github.com/codecrafters-io/tester-utils/test_case_harness"
+)
+
+func testMultipleMatchesMultipleLines(stageHarness *test_case_harness.TestCaseHarness) error {
+	utils.RelocateSystemGrep(stageHarness)
+
+	animals := random.RandomElementsFromArray(utils.ANIMALS, 2)
+	fruits := random.RandomElementsFromArray(utils.FRUITS, 2)
+
+	animal1 := animals[0]
+	animal2 := animals[1]
+	fruit1 := fruits[0]
+	fruit2 := fruits[1]
+
+	testCaseCollection := test_cases.PrintMatchesOnlyTestCaseCollection{
+		{
+			Pattern: `\d`,
+			InputLines: []string{
+				"a1b",
+				"no digits here",
+				"2c3d",
+			},
+			ExpectedExitCode: 0,
+		},
+		{
+			Pattern: fmt.Sprintf(`(%s|%s)`, fruit1, fruit2),
+			InputLines: []string{
+				fmt.Sprintf("I like %s", fruit1),
+				"nothing here",
+				fmt.Sprintf("%s and %s are tasty", fruit2, fruit1),
+			},
+			ExpectedExitCode: 0,
+		},
+		{
+			Pattern: `XYZ123`,
+			InputLines: []string{
+				"abc",
+				"def",
+				"ghi",
+			},
+			ExpectedExitCode: 1,
+		},
+		{
+			Pattern: `cat`,
+			InputLines: []string{
+				"dogdogdog",
+				"elephant",
+				"cat-cat-dog",
+			},
+			ExpectedExitCode: 0,
+		},
+		{
+			Pattern: fmt.Sprintf(`I saw \d+ (%s|%s)s?`, animal1, animal2),
+			InputLines: []string{
+				fmt.Sprintf("Yesterday I saw 3 %ss.", animal1),
+				"Nothing interesting today.",
+				fmt.Sprintf("Last week I saw 12 %ss.", animal2),
+			},
+			ExpectedExitCode: 0,
+		},
+		{
+			Pattern: `cats and dogs`,
+			InputLines: []string{
+				"today is sunny",
+				"no rains here",
+				"tomorrow maybe rainy",
+			},
+			ExpectedExitCode: 1,
+		},
+	}
+
+	return testCaseCollection.Run(stageHarness)
+}
---
+++ internal/test_cases/print_matches_only_test_case.go
@@ -0,0 +1,69 @@
+package test_cases
+
+import (
+	"fmt"
+	"path"
+	"strings"
+
+	"github.com/codecrafters-io/grep-tester/internal/assertions"
+	"github.com/codecrafters-io/grep-tester/internal/grep"
+	"github.com/codecrafters-io/tester-utils/test_case_harness"
+)
+
+type PrintMatchesOnlyTestCase struct {
+	Pattern          string
+	InputLines       []string
+	ExpectedExitCode int
+}
+
+type PrintMatchesOnlyTestCaseCollection []PrintMatchingLinesTestCase
+
+func (c PrintMatchesOnlyTestCaseCollection) Run(stageHarness *test_case_harness.TestCaseHarness) error {
+	logger := stageHarness.Logger
+	executable := stageHarness.Executable
+
+	executable.TimeoutInMilliseconds = 1000 * 3600
+
+	for _, testCase := range c {
+		// Run executable and collect result
+		allInputLines := strings.Join(testCase.InputLines, "\n")
+		logger.Infof("$ echo -ne %q | ./%s -o -E '%s'", allInputLines, path.Base(executable.Path), testCase.Pattern)
+
+		grepResult := grep.EmulateGrep([]string{"-o", "-E", testCase.Pattern}, []byte(allInputLines))
+		actualResult, err := executable.RunWithStdin([]byte(allInputLines), "-o", "-E", testCase.Pattern)
+
+		if err != nil {
+			return err
+		}
+
+		// Compare against grep
+		if testCase.ExpectedExitCode != grepResult.ExitCode {
+			panic(fmt.Sprintf("CodeCrafters Internal Error: Expected exit code %v, grep returned %v", testCase.ExpectedExitCode, grepResult.ExitCode))
+		}
+
+		// Run assertions
+		exitCodeAssertion := assertions.ExitCodeAssertion{
+			ExpectedExitCode: testCase.ExpectedExitCode,
+		}
+
+		if err := exitCodeAssertion.Run(actualResult, logger); err != nil {
+			return err
+		}
+
+		expectedOutput := strings.TrimSpace(string(grepResult.Stdout))
+
+		expectedOutputLines := strings.FieldsFunc(expectedOutput, func(r rune) bool {
+			return r == '\n'
+		})
+
+		orderedLinesAssertion := assertions.OrderedLinesAssertion{
+			ExpectedOutputLines: expectedOutputLines,
+		}
+
+		if err := orderedLinesAssertion.Run(actualResult, logger); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
