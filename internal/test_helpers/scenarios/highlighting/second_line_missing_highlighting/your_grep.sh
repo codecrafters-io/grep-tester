4d5bbbc0b54e380c8d536099d5b9197c8083aa38
Udeshya Dhungana <075bct095.udeshya@pcampus.edu.np>
2025-12-15 12:29:54 +0545

    Preserve exit code in 'second_line_missing_highlighting'

--- internal/test_helpers/scenarios/highlighting/second_line_missing_highlighting/your_grep.sh
+++ internal/test_helpers/scenarios/highlighting/second_line_missing_highlighting/your_grep.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Run your_grep.sh and capture its exit code
+output="$("$(dirname "$0")/../../../pass_all/your_grep.sh" "$@" "--color=always")"
+exit_code=$?
+
 # Remove ansi from all lines except the first
-"$(dirname "$0")/../../../pass_all/your_grep.sh" "$@" "--color=always" | awk 'NR==1 { print; next } { gsub(/\x1b\[[0-9;]*m/, ""); print }'
+echo "$output" | awk 'NR==1 { print; next } { gsub(/\x1b\[[0-9;]*m/, ""); print }'
+
+# Exit with the original exit code
+exit $exit_code
